215dc8dd80e79604d3a05e0b91419d6d65f06254
соня <sonyakt@bk.ru>
2021-04-21 23:30:22 +0300

    add functional

--- server.py
+++ server.py
@@ -1,26 +1,25 @@
 from flask import Flask, request
-from utils import TMP_DIR, get_room_posibilities
-from pathlib import Path
-from uuid import uuid4
+from utils import save_target_image, RoomDeterminant
 
 app = Flask("WPI backend")
 
+
 @app.route('/', methods=["POST"])
 def determine_room_route():
-	if 'target' not in request.files:
-		return "No file part.", 400
+    if 'target' not in request.files:
+        return "No file part.", 400
 
-	target = request.files['target']
-	if target.filename == '':
-		return "No target photo passed.", 400
+    target = request.files['target']
+    if target.filename == '':
+        return "No target photo passed.", 400
 
-	Path(TMP_DIR).mkdir(exist_ok=True)
-	target.filename = str(uuid4()) + '.jpg'
-	target.save(TMP_DIR)
+    print(target, type(target))
+    save_target_image(target)
 
-	room_posibilities = get_room_posibilities(target.filename)
-    #TODO: delete from TMP
-	return room_posibilities, 200
+    determinant = RoomDeterminant()
+    room_possibilities = determinant.get_room_possibilities(target.filename)
+    return room_possibilities, 200
+
 
 if __name__ == '__main__':
-	app.run()
+    app.run(use_reloader=True)
--- utils.py
+++ utils.py
@@ -6,7 +6,9 @@ import torch.optim as optim
 
 import numpy as np
 import pandas as pd
+from pathlib import Path
 from PIL import Image
+from uuid import uuid4
 
 import os
 
@@ -15,72 +17,102 @@ TMP_DIR = os.path.join(PROJ_ROOT, 'TMP')
 ROOMS_DIR = os.path.join(PROJ_ROOT, 'rooms')
 
 
+def save_target_image(image):
+    Path(TMP_DIR).mkdir(exist_ok=True)
+    image.filename = str(uuid4()) + '.jpg'
+    image.save(os.path.join(TMP_DIR, image.filename))
+
+
 class RoomDeterminant:
     def __init__(self):
-        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device(
+            'cuda:0' if torch.cuda.is_available() else 'cpu')
         self.classifier = self._create_classifier()
-       
+
     def _create_classifier(self):
-        resnet18 = models.resnet18(pretrained = True)
+        resnet18 = models.resnet18(pretrained=True)
 
         classifier = nn.Sequential(
             resnet18,
-            nn.ReLU(inplace = True),
+            nn.ReLU(inplace=True),
             nn.Linear(1000, 2),
             nn.Sigmoid()
         )
 
         classifier.to(self.device)
-        classifier.load_state_dict(torch.load('classifier.pt', map_location = self.device))
+        classifier.load_state_dict(torch.load(
+            'classifier.pt', map_location=self.device))
         return classifier
 
-    def determine(self, images):
-        '''
-        in: images - list, список путей к фотографиям
-        out: 0 or 1 (0 - doesn't match, 1 - matchs)
-        '''
+    def get_room_possibilities(self, target_name: str) -> dict:
+        """
+        Args:
+            target_name: str - name of a target file
+        Returns:
+            dict of possibilities:
+            {
+                room-name : bool,
+                ...
+            }
+        """
+        possibilities = {}
+        target_full_path = os.path.join(TMP_DIR, target_name)
+
+        rooms_images = self._extract_rooms_images()
+
+        for room_name, images in rooms_images.items():
+            possibilities[room_name] = self._is_correct_room(
+                images,
+                target_full_path,
+            )
+
+        return possibilities
+
+    @staticmethod
+    def _extract_rooms_images():
+        """ Extracts room images from 'rooms/' directory """
+        rooms_images = {}
+        _, roomnames, _ = next(os.walk(ROOMS_DIR))
+        for roomname in roomnames:
+            room_dir, _, room_images = next(
+                os.walk(os.path.join(ROOMS_DIR, roomname)))
+
+            images = [os.path.join(room_dir, im) for im in room_images]
+            rooms_images[roomname] = images
+        return rooms_images
+
+    def _is_correct_room(self, images, target) -> bool:
+        """
+        Args:
+            images: list[str], paths to room photos
+            target: str - path to target
+        Returns:
+            bool: weather room mathc target
+
+        """
+        images = [target] + images
         self.classifier.eval()
         images = pd.Series(images)
         # return images
         images = images.apply(lambda elem: Image.open(elem))
         images = images.apply(lambda elem: elem.convert('RGB'))
         transform = transforms.Compose([
-                        transforms.Resize((620, 620)),
-                        transforms.RandomHorizontalFlip(p=0.5),
-                        transforms.RandomVerticalFlip(p=0.5),
-                        transforms.RandomRotation(30),
-                        transforms.ToTensor(),
-                        # Use mean and std for pretrained models
-                        # https://pytorch.org/docs/stable/torchvision/models.html
-                        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                        std=[0.229, 0.224, 0.225])
-                ])
+            transforms.Resize((620, 620)),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomVerticalFlip(p=0.5),
+            transforms.RandomRotation(30),
+            transforms.ToTensor(),
+            # Use mean and std for pretrained models
+            # https://pytorch.org/docs/stable/torchvision/models.html
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        ])
         images = images.apply(lambda elem: transform(elem))
         images = torch.stack(tuple(images)).view(3, 620*5, 620*3)
 
         pred = self.classifier(images.to(self.device).unsqueeze(0))
-        pred = torch.argmax(pred, axis = 1)
-        return int(pred)
-
-
-def get_room_posibilities(target_name: str) -> dict:
-    determinant = RoomDeterminant()
-    room_images = {}
-    posibilities = {}
-
-    target_full_path = os.path.join(TMP_DIR, target_name)
-
-    dirname, roomnames, _ = os.walk(ROOMS_DIR)
-    for roomname in roomnames:
-        _, _, room_images = os.walk(os.path.join(dirname,roomname))
-        images = [target_full_path]
-        images += room_images
-        room_images[roomname] = images
-
-    for room_name, images in room_images.items():
-        posibilities[room_name] = determinant.determine(images)
-
-    return posibilities
+        pred = torch.argmax(pred, axis=1)
+        return bool(pred)
 
 
 
